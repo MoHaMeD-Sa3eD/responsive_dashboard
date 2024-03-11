0bff818e86cfb59d8b33d1ab4328ac5665f82d3e
MoHaMeD-Sa3eD <mahmedsaed73@gmail.com>
2024-03-11 17:27:37 +0200

    added all app assets

---
+++ lib/ui_part/customWidgets/utils/app_images.dart
@@ -0,0 +1,26 @@
+abstract class AppImages {
+  AppImages._();
+
+  static const String assetsImageAvatar1 = 'assets/images/avatar_1.svg';
+  static const String assetsImageAvatar2 = 'assets/images/avatar_2.svg';
+  static const String assetsImageAvatar3 = 'assets/images/avatar_3.svg';
+  static const String assetsImagesBalance = 'assets/images/balance.svg';
+
+  static const String assetsImagesCardBackground = 'assets/images/card_background.svg';
+
+  static const String assetsImagesDashboard = 'assets/images/dashboard.svg';
+  static const String assetsImagesExpenses = 'assets/images/expenses.svg';
+  static const String assetsImagesGallery = 'assets/images/gallery.svg';
+  static const String assetsImagesIncome = 'assets/images/income.svg';
+
+  static const String assetsImagesLogout = 'assets/images/logout.svg';
+
+  static const String assetsImagesMyInvestments = 'assets/images/my_investments.svg';
+  static const String assetsImagesMyTransactions = 'assets/images/my_transactions.svg';
+
+  static const String assetsImagesSettings = 'assets/images/settings.svg';
+
+  static const String assetsImagesStatistics = 'assets/images/statistics.svg';
+
+  static const String assetsImagesWalletAccount = 'assets/images/wallet_account.svg';
+}
