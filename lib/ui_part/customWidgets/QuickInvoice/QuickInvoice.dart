968a401eb8102585ebfd64bf892c3195256f64e5
MoHaMeD-Sa3eD <mahmedsaed73@gmail.com>
2024-03-14 14:25:08 +0200

    created custom TextField and Custom titleTextField

--- lib/ui_part/customWidgets/QuickInvoice/QuickInvoice.dart
+++ lib/ui_part/customWidgets/QuickInvoice/QuickInvoice.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard_flutter/ui_part/customWidgets/CustomBackGroundContainer.dart';
-import 'package:responsive_dashboard_flutter/ui_part/customWidgets/QuickInvoice/QuickInvoiceForm/TitleTextFieldModel.dart';
+import 'package:responsive_dashboard_flutter/ui_part/customWidgets/QuickInvoice/QuickInvoiceForm/QuickInvoiceForm.dart';
 import 'package:responsive_dashboard_flutter/ui_part/customWidgets/QuickInvoice/QuickInvoiceHeader/QuickInvoiceHeader.dart';
 import 'LatestTransactionSection/LatestTransaction.dart';
-import 'QuickInvoiceForm/TitleTextField.dart';
 
 class QuickInvoice extends StatelessWidget {
   const QuickInvoice({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomBackGroundContainer(
+    return const CustomBackGroundContainer(
       child: Column(
         children: [
-          const QuickInvoiceHeader(),
-          const SizedBox(height: 24),
-          const LatestTransaction(),
-          const Divider(height: 48),
-          TitleTextField(
-            titleTextFieldModel: TitleTextFieldModel(
-                hintText: 'hintText', titleText: 'titleText'),
-          ),
+          QuickInvoiceHeader(),
+          SizedBox(height: 24),
+          LatestTransaction(),
+          Divider(height: 48),
+          QuickInvoiceForm(),
         ],
       ),
     );
---
+++ lib/ui_part/customWidgets/QuickInvoice/QuickInvoiceForm/QuickInvoiceForm.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class QuickInvoiceForm extends StatelessWidget {
+  const QuickInvoiceForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
