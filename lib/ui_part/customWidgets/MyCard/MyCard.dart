6ae6f6c7e84843da7e8daa7ac5761202c0c01e96
MoHaMeD-Sa3eD <mahmedsaed73@gmail.com>
2024-03-14 16:43:06 +0200

    fixed cardBackGroundImage

--- lib/ui_part/customWidgets/MyCard/MyCard.dart
+++ lib/ui_part/customWidgets/MyCard/MyCard.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_dashboard_flutter/ui_part/utils/app_images.dart';
 
 class MyCard extends StatelessWidget {
@@ -11,12 +10,12 @@ class MyCard extends StatelessWidget {
       aspectRatio: 420 / 215,
       child: Container(
         decoration: ShapeDecoration(
+          image: const DecorationImage(image: AssetImage(AppImages.assetsImagesCardBackground)),
           color: const Color(0xFF4EB7F2),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
         ),
-        child: SvgPicture.asset(AppImages.assetsImagesCardBackground),
       ),
     );
   }
