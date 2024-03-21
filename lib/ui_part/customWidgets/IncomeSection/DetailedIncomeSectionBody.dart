e8ada5245ac6c429ac7ef4b915f1b4b01acf7f2c
MoHaMeD-Sa3eD <mahmedsaed73@gmail.com>
2024-03-21 14:31:18 +0200

    simple code Refactor

--- lib/ui_part/customWidgets/IncomeSection/DetailedIncomeSectionBody.dart
+++ lib/ui_part/customWidgets/IncomeSection/DetailedIncomeSectionBody.dart
@@ -1,4 +1,3 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 import 'IncomeChart.dart';
@@ -21,63 +20,4 @@ class DetailedIncomeSectionBody extends StatelessWidget {
   }
 }
 
-class MiniIncomeChart extends StatefulWidget {
-  const MiniIncomeChart({super.key});
 
-  @override
-  State<MiniIncomeChart> createState() => _MiniIncomeChartState();
-}
-
-class _MiniIncomeChartState extends State<MiniIncomeChart> {
-  int activeIndex = -1;
-
-  @override
-  Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1,
-      child: PieChart(
-        getChartData(),
-      ),
-    );
-  }
-
-  PieChartData getChartData() {
-    return PieChartData(
-      pieTouchData: PieTouchData(
-          enabled: true,
-          touchCallback: (p0, pieTouchResponse) {
-            setState(() {
-              activeIndex =
-                  pieTouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
-            });
-          }),
-      sectionsSpace: 0,
-      sections: [
-        PieChartSectionData(
-          radius: activeIndex == 0 ? 50 : 60,
-          showTitle: false,
-          value: 40,
-          color: const Color(0xFF208Bc7),
-        ),
-        PieChartSectionData(
-          radius: activeIndex == 1 ? 50 : 60,
-          showTitle: false,
-          value: 25,
-          color: const Color(0xFF4DB7F2),
-        ),
-        PieChartSectionData(
-          radius: activeIndex == 2 ? 50 : 60,
-          showTitle: false,
-          value: 20,
-          color: const Color(0xFF064060),
-        ),
-        PieChartSectionData(
-          radius: activeIndex == 3 ? 50 : 60,
-          showTitle: false,
-          value: 22,
-          color: const Color(0xFFE2DECD),
-        ),
-      ],
-    );
-  }
-}
--- lib/ui_part/customWidgets/IncomeSection/MiniIncomeSectionBody.dart
+++ lib/ui_part/customWidgets/IncomeSection/MiniIncomeSectionBody.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'MiniDetailedIncomeChart.dart';
+
+
+
 class MiniIncomeSectionBody extends StatelessWidget {
   const MiniIncomeSectionBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('mini Chart');
+    return const MiniDetailedIncomeChart();
   }
 }
